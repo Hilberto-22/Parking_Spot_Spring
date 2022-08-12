d02fe8254d28dfd3dab482bcaae496b7fbb5a0c5
hilberto <betogouveia1@outlook.com>
2022-08-12 14:26:32 -0300

    Criacao do controller, dto, service e repository para salvar

--- src/main/java/com/example/parkingspot/spring/services/ParkingSpotService.java
+++ src/main/java/com/example/parkingspot/spring/services/ParkingSpotService.java
@@ -1,14 +1,22 @@
 package com.example.parkingspot.spring.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.parkingspot.spring.models.ParkingSpotModel;
 import com.example.parkingspot.spring.repository.ParkingSpotRepository;
 
 @Service
 public class ParkingSpotService {
     
-    ParkingSpotRepository parkingSpotRepository;
+   final ParkingSpotRepository parkingSpotRepository;
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
